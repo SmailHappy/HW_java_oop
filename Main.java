f8b18bebbae3532944ee057c6ceaf6feeb9df983
Alexey <alex-ey98@mail.ru>
2023-02-16 04:52:55 +0300

    Домашнее задание 1

--- Main.java
+++ Main.java
@@ -4,17 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Tree tree = new Tree();
         Scanner iScanner = new Scanner(System.in, "Cp866");
-        System.out.println("Добро пожаловать в генеалогическое древо.");
+        System.out.println("Добро пожаловать в генеалогическое древо.\n");
         Boolean flag = true;
+
         while (flag) {
             System.out.println("1. Показать древо\n2. Добавить человека\n3. Поиск человека по имени\n4. Завершить.");
             System.out.print("Выберите действие : ");
             String choice = iScanner.next();
 
-            if (choice.equals("1")) System.out.println(tree.getTree());
+            if (choice.equals("1")) {
+                System.out.println();
+                System.out.println(tree.getTree());
 
-            else if (choice.equals("2")) {
-                System.out.print("Введите имя : ");
+            } else if (choice.equals("2")) {
+                System.out.print("\nВведите имя : ");
                 String name = iScanner.next();
                 System.out.print("Введите пол : ");
                 String sex = iScanner.next();
@@ -22,21 +25,27 @@ public class Main {
                 String name_father = iScanner.next();
                 System.out.print("Введите имя матери (если не известно 'нет') : ");
                 String name_mother = iScanner.next();
+
                 if (name_father.equalsIgnoreCase("нет") || name_mother.equalsIgnoreCase("нет")) {
                     tree.add(new Human(name, sex));
                 } else tree.add(new Human(name, sex, name_father, name_mother));
-                System.out.println(name + " Успешно добавлен в древо.");
+
+                System.out.println(name + " Успешно добавлен в древо.\n");
 
             } else if (choice.equals("3")) {
-                System.out.print("Введите имя : ");
+                System.out.print("\nВведите имя : ");
                 String name = iScanner.next();
                 System.out.println(tree.getInfoHuman(name));
-                System.out.println("1. Показать детей\n2. Показать мать\n3. Показать отца\n4. В основное меню");
+                System.out.println("\n1. Показать детей\n2. Показать мать\n3. Показать отца\n4. В основное меню");
                 System.out.print("Выберите действие : ");
                 choice = iScanner.next();
-                if (choice.equals("1")) tree.getInfoChildren(name);
-                else if (choice.equals("2")) tree.getInfoMother(name);
-                else if (choice.equals("3")) tree.getInfoFather(name);
+                
+                if (choice.equals("1")) {
+                    System.out.println();
+                    System.out.println(tree.getInfoChildren(name));
+                }
+                else if (choice.equals("2")) System.out.println("\n" + tree.getInfoMother(name) + "\n");
+                else if (choice.equals("3")) System.out.println("\n" + tree.getInfoFather(name) + "\n");
                 
             } else flag = false;
         }
